c4d5ce9ebf4524cbd93208ea16eebcf05340fc1a
Victor Tseng <palatis@gmail.com>
2017-07-08 20:58:21 +0800

    guard against null listener

--- library/src/main/java/com/malmstein/fenster/factory/AndroidMediaPlayerFactory.java
+++ library/src/main/java/com/malmstein/fenster/factory/AndroidMediaPlayerFactory.java
@@ -71,72 +71,93 @@ public class AndroidMediaPlayerFactory implements MediaPlayerFactory {
     private static class AndroidMediaPlayer extends MediaPlayer implements IMediaPlayer {
         @Override
         public void setOnPreparedListener(final IOnPreparedListener listener) {
-            setOnPreparedListener(new OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    listener.onPrepared((IMediaPlayer) mp);
-                }
-            });
+            setOnPreparedListener(
+                    listener == null ?
+                            null :
+                            new OnPreparedListener() {
+                                @Override
+                                public void onPrepared(MediaPlayer mp) {
+                                    listener.onPrepared((IMediaPlayer) mp);
+                                }
+                            });
         }
 
         @Override
         public void setOnVideoSizeChangedListener(final IOnVideoSizeChangedListener listener) {
-            setOnVideoSizeChangedListener(new OnVideoSizeChangedListener() {
-                @Override
-                public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
-                    listener.onVideoSizeChanged((IMediaPlayer) mp, width, height);
-                }
-            });
+            setOnVideoSizeChangedListener(
+                    listener == null ?
+                            null :
+                            new OnVideoSizeChangedListener() {
+                                @Override
+                                public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                                    listener.onVideoSizeChanged((IMediaPlayer) mp, width, height);
+                                }
+                            });
         }
 
         @Override
         public void setOnCompletionListener(final IOnCompletionListener listener) {
-            setOnCompletionListener(new OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    listener.onCompletion((IMediaPlayer) mp);
-                }
-            });
+            setOnCompletionListener(
+                    listener == null ?
+                            null :
+                            new OnCompletionListener() {
+                                @Override
+                                public void onCompletion(MediaPlayer mp) {
+                                    listener.onCompletion((IMediaPlayer) mp);
+                                }
+                            });
         }
 
         @Override
         public void setOnErrorListener(final IOnErrorListener listener) {
-            setOnErrorListener(new OnErrorListener() {
-                @Override
-                public boolean onError(MediaPlayer mp, int what, int extra) {
-                    return listener.onError((IMediaPlayer) mp, what, extra);
-                }
-            });
+            setOnErrorListener(
+                    listener == null ?
+                            null :
+                            new OnErrorListener() {
+                                @Override
+                                public boolean onError(MediaPlayer mp, int what, int extra) {
+                                    return listener.onError((IMediaPlayer) mp, what, extra);
+                                }
+                            });
         }
 
         @Override
         public void setOnInfoListener(final IOnInfoListener listener) {
-            setOnInfoListener(new OnInfoListener() {
-                @Override
-                public boolean onInfo(MediaPlayer mp, int what, int extra) {
-                    return listener.onInfo((IMediaPlayer) mp, what, extra);
-                }
-            });
+            setOnInfoListener(
+                    listener == null ?
+                            null :
+                            new OnInfoListener() {
+                                @Override
+                                public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                                    return listener.onInfo((IMediaPlayer) mp, what, extra);
+                                }
+                            });
         }
 
         @Override
         public void setOnBufferingUpdateListener(final IOnBufferingUpdateListener listener) {
-            setOnBufferingUpdateListener(new OnBufferingUpdateListener() {
-                @Override
-                public void onBufferingUpdate(MediaPlayer mp, int percent) {
-                    listener.onBufferingUpdate((IMediaPlayer) mp, percent);
-                }
-            });
+            setOnBufferingUpdateListener(
+                    listener == null ?
+                            null :
+                            new OnBufferingUpdateListener() {
+                                @Override
+                                public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                                    listener.onBufferingUpdate((IMediaPlayer) mp, percent);
+                                }
+                            });
         }
 
         @Override
         public void setOnSeekCompleteListener(final IOnSeekCompleteListener listener) {
-            setOnSeekCompleteListener(new OnSeekCompleteListener() {
-                @Override
-                public void onSeekComplete(MediaPlayer mp) {
-                    listener.onSeekComplete((IMediaPlayer) mp);
-                }
-            });
+            setOnSeekCompleteListener(
+                    listener == null ?
+                            null :
+                            new OnSeekCompleteListener() {
+                                @Override
+                                public void onSeekComplete(MediaPlayer mp) {
+                                    listener.onSeekComplete((IMediaPlayer) mp);
+                                }
+                            });
         }
     }
 }
