afe36094c58e4442b1447850667438693e6f8a17
Victor Tseng <palatis@gmail.com>
2017-07-08 20:10:36 +0800

    fix BrightnessHelper fail on Api23 due to new permission model

--- library/src/main/java/com/malmstein/fenster/helper/BrightnessHelper.java
+++ library/src/main/java/com/malmstein/fenster/helper/BrightnessHelper.java
@@ -1,19 +1,24 @@
 package com.malmstein.fenster.helper;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 
 public class BrightnessHelper {
 
-    public static void setBrightness(Context context, int brightness){
-        ContentResolver cResolver = context.getContentResolver();
-        Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+    public static void setBrightness(Context context, int brightness) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_SETTINGS) == PackageManager.PERMISSION_GRANTED) {
+            final ContentResolver cResolver = context.getContentResolver();
+            Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+        }
     }
 
     public static int getBrightness(Context context) {
-        ContentResolver cResolver = context.getContentResolver();
         try {
+            final ContentResolver cResolver = context.getContentResolver();
             return Settings.System.getInt(cResolver, Settings.System.SCREEN_BRIGHTNESS);
         } catch (Settings.SettingNotFoundException e) {
             return 0;
